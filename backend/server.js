cb4d642b2b822f30ba6fff7e02a0497bbb6bb06d
Emmanuelikeh <88993873+Emmanuelikeh@users.noreply.github.com>
2022-09-28 18:55:12 -0500

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,142 @@
+console.log("happy");
+const express = require('express');
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = express();
+
+app.use(cors())
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
+
+
+
+let carsMockData = [
+    {
+        "id": 1,
+        "brand": "Hyundai",
+        "name": "Ioniq",
+        "releaseYear": 2017,
+        "color": "blue"
+    },
+    {
+        "id": 2,
+        "brand": "Toyota",
+        "name": "Prius",
+        "releaseYear": 2007,
+        "color": "blue"
+    },
+    {
+        "id": 3,
+        "brand": "Chevrolet",
+        "name": "Aveo",
+        "releaseYear": 2007,
+        "color": "white"
+    },
+    {
+        "id": 4,
+        "brand": "BMW",
+        "name": "M5",
+        "releaseYear": 2017,
+        "color": "White"
+    },
+    {
+        "id": 5,
+        "brand": "Tesla",
+        "name": "S",
+        "releaseYear": 2019,
+        "color": "Black"
+    }
+]
+
+app.get("/list", (req, res) => {
+    res.send(carsMockData)
+})
+
+app.delete("/delete", (req, res) => {
+    // carsMockData.forEach((item) => {
+    //     if(item.id === req.body.id){
+    //         res.status(500).send('No car with give id exists')
+    //         return;
+    //     }
+    // })
+
+    let foundCarWithId = carsMockData.some(car => car.id === parseInt(req.body.id))
+
+    if(foundCarWithId){
+        carsMockData = carsMockData.filter((car) => car.id !== req.body.id) 
+        res.send(carsMockData);
+    }
+    else{
+        res.status(500).send('No car with give id exists')
+    }
+})
+
+
+/** Create POST API. Get the new car data from react. 
+ *      Check if car with id exists. If Yes return 500. With message 'Car already exists'
+ *      If there is no car with the id, add the new car to  carsMockData and return carsMockData as response */
+
+
+
+
+
+/** Create PUT API. 
+ *  Check if car with id exists. If No return 500 with error 'No car with given id exist'. 
+ *  If there is car with the requested id, update that car's data in 'carsMockData' and return 'carsMockData' */
+
+
+
+
+/** Create Delete API. 
+ *  Check if car with id exists. If No return 500. With message 'No car with give id exists'
+ *  If there is car with the requested id. Delete that car from 'carsMockData' and return 'carsMockData'
+*/
+
+app.post("/save", (req,res) => {
+    let foundCarWithId = carsMockData.some(car => car.id === parseInt(req.body.id))
+    if(foundCarWithId){
+        res.status(500).send("Car already exists")
+    }
+    else{
+        let id = parseInt(req.body.id);
+        let brand = req.body.brand;
+        let name = req.body.name;
+        let releaseYear = req.body.releaseYear;
+        let color = req.body.color;
+        carsMockData.push({"id":id, "brand":brand, "name":name, "releaseYear": releaseYear, "color": color});
+        res.send(carsMockData);
+    }
+    
+})
+
+app.put("/edit", (req,res) => {
+    let foundCarWithId = carsMockData.some(car => car.id === parseInt(req.body.id))
+    if(foundCarWithId){
+        let id = parseInt(req.body.id);
+        let brand = req.body.brand;
+        let name = req.body.name;
+        let releaseYear = req.body.releaseYear;
+        let color = req.body.color;
+        carsMockData.push({"id":id, "brand":brand, "name":name, "releaseYear": releaseYear, "color": color});
+        res.send(carsMockData);
+    }
+    else{
+        res.status(500).send('No car with given id exist')
+    }
+})
+
+
+
+
+
+
+
+app.listen(8000);
---
+++ front-end/src/App.js
@@ -0,0 +1,162 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function Cars() {
+/**
+ * Fields required for the car
+      "id",
+      "brand",
+      "name",
+      "releaseYear",
+      "color"
+ */
+  const carFormInitialData = {
+    id: 0,
+    brand:'',
+    name: '',
+    releaseYear: 0,
+    color: ''
+  }
+  const [carFormData, setCarFormData] = useState(carFormInitialData);
+  const [carData, setCarData] = useState([]);
+
+
+
+
+  useEffect(() => {
+    fetch('http://localhost:8000/list')
+    .then(res  => res.json())
+    .then((result) => {
+      setCarData(result);
+    })
+  }, [])
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setCarFormData({
+      ...carFormData,
+      [name]: value,
+    });
+  }
+
+  const handleSubmit = (event) => {
+    /**
+     * Gather all the form data to state variable carFormData
+     * When the form is submitted POST the data to Backend using fetch post
+     * https://googlechrome.github.io/samples/fetch-api/fetch-post.html
+     */
+    event.preventDefault();
+    fetch("http://localhost:8000/save", {
+      method: "POST",
+      body : JSON.stringify({
+        id: event.target.id.value,
+        brand: event.target.brand.value,
+        name: event.target.name.value,
+        releaseYear: event.target.releaseYear.value,
+        color: event.target.color.value
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    }).then(res => res.json()).then((result) => {
+      setCarData(result)
+    })
+
+    setCarFormData(carFormInitialData);
+  }
+
+
+  
+
+  const handleDelete = (id) => {
+    fetch("http://localhost:8000/delete", {
+      method: "DELETE",
+      body: JSON.stringify({
+        id:id
+      }),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    }).then(res => res.json()).then((result) => {
+      setCarData(result)
+    })
+    /**
+     * When clicked on a delete button, get the id of the car's delete button clicked
+     * Then use javascript fetch to send DELETE request to NodeJS
+     * https://openjavascript.info/2022/01/03/using-fetch-to-make-get-post-put-and-delete-requests/
+     */
+  }
+
+/** 🥳🥳🥳🥳🥳🥳 DOUBLE BONUS POINTS 🥳🥳🥳🥳🥳🥳 */
+  const handleEdit = () => {
+    /**
+     * When clicked on a edit button figure out a way to edit the car data.
+     * Once edited send the updated data to NodeJS.
+     * Then use javascript fetch to send DELETE request to NodeJS
+     * https://openjavascript.info/2022/01/03/using-fetch-to-make-get-post-put-and-delete-requests/
+     */
+  }
+ 
+  return (
+    <div className='cars-from-wrapper'>
+      <form id="cars-form" onSubmit={handleSubmit} autoComplete="off">
+        {/** 
+           * TODO: Update the form fields with inputs for 
+           *    ID, Brand, Name, ReleaseYear and Color
+           * Make required changes to  const carFormInitialData
+           * */}  
+        <label>
+          ID:
+          <input name='id' type="text" value={carFormData.id} onChange={handleInputChange} />
+        </label>
+        <label>
+          Brand:
+          <input name='brand' type="text" value={carFormData.brand} onChange={handleInputChange} />
+        </label>
+        <label>
+          Name:
+          <input name='name' type="text" value={carFormData.name} onChange={handleInputChange} />
+        </label>
+        <label>
+          ReleaseYear:
+          <input name='releaseYear' type="text" value={carFormData.releaseYear} onChange={handleInputChange} />
+        </label>
+        <label>
+          Color:
+          <input name='color' type="text" value={carFormData.color} onChange={handleInputChange} />
+        </label>
+        <input type="submit" value="Submit" id="submit" action="submit" />
+      </form>
+       {/** 
+           * TODO: Update the code below to see any new proprties added to carFormData
+           * */}  
+      <p>ID:{carFormData.id}, brand:{carFormData.brand}, name:{carFormData.name}, ReleaseYear: {carFormData.releaseYear}, Color:{carFormData.color} </p>
+
+      <h2>Cars Data</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Car Brand</th>
+            <th>Car Name</th>
+            <th>Release Year</th>
+            <th> Car Color</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/** 
+           * TODO: Replace this code with Data from Node JS GET api data
+           * React documentation: https://reactjs.org/docs/lists-and-keys.html
+           * How to get data from API: https://www.w3schools.com/jsref/api_fetch.asp
+           * */}
+           {carData.map((car, index)=>{
+            return <tr key={index}><td>{car.id}</td><td>{car.brand}</td><td>{car.name}</td><td>{car.releaseYear}</td><td>{car.color}</td><td>✎</td><td onClick={() => handleDelete(car.id)}>🗑</td></tr>
+           })}          
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Cars;
+
